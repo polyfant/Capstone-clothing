f4a6626242d521b8c65efe96218f51ba397f6391
Jonas <polyfant@live.se>
2024-05-12 20:01:10 +0200

    något krångel, tittar vidare imorgon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
-import Navigation from './routes/navigation/navigation.component.jsx';
-import Home from './routes/home/home.component.jsx';
-import Authentication from './routes/authentication/authentication.component.jsx';
-import Shop from './routes/shop/shop.component.jsx';
-import Checkout from './routes/checkout/checkout.component.jsx';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
-        <Route path='/' index element={<Home />} />
-        <Route path='shop' index element={<Shop />} />
+        <Route index element={<Home />} />
+        <Route path='shop/*' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
         <Route path='checkout' element={<Checkout />} />
       </Route>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-
-import {
-  CategoriesProvider,
-  categoriesContext,
-} from './contexts/categories.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+
 import './index.scss';
 
 const rootElement = document.getElementById('root');
