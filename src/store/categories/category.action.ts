4f8f054f2f54fe660638fda3097da763b525a776
Jonas <polyfant@live.se>
2024-05-30 12:08:12 +0200

    started to migrate to typescript

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -2,41 +2,42 @@ import { CATEGORIES_ACTION_TYPES, Category } from './category.types';
 import {
   createAction,
   Action,
-  ActionWithPayLoad,
+  ActionWithPayload,
+  withMatcher,
 } from '../../utils/reducer/reducer.utils';
 
 export type fetchCategoriesStart =
   Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
-export type fetchCategoriesSuccess = ActionWithPayLoad<
+export type fetchCategoriesSuccess = ActionWithPayload<
   CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
   Category[]
 >;
 
-export type fetchCategoriesFailed = ActionWithPayLoad<
+export type fetchCategoriesFailed = ActionWithPayload<
   CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
   Error
 >;
-export type CategoryAction =
-  | fetchCategoriesStart
-  | fetchCategoriesSuccess
-  | fetchCategoriesFailed;
 
-export const fetchCategoriesStart = (): fetchCategoriesStart =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+export const fetchCategoriesStart = withMatcher(
+  (): fetchCategoriesStart =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+);
 
-export const fetchCategoriesSuccess = (
-  categoriesArray: Category[]
-): fetchCategoriesSuccess =>
-  createAction(
-    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
-    categoriesArray
-  );
+export const fetchCategoriesSuccess = withMatcher(
+  (categoriesArray: Category[]): fetchCategoriesSuccess =>
+    createAction(
+      CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+      categoriesArray
+    )
+);
 
-export const fetchCategoriesFailed = (error: Error): fetchCategoriesFailed =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+export const fetchCategoriesFailed = withMatcher(
+  (error: Error): fetchCategoriesFailed =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
+);
 
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+// export const setCategories = (categoriesArray) =>
+//   createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
 
 // export const fetchCategoriesAsync = () => async (dispatch) => {
 //   dispatch(fetchCategoriesStart());
